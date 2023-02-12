0796d46301fc25a2cc282bff1659ef9063345a46
Kateryna Verveda <katushka211@gmail.com>
2023-02-12 23:02:02 +0100

    add tasks condition

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,5 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
 const inputField = document.querySelector("#name-input");
 const outputField = document.querySelector("#name-output");
 
--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,15 @@
+// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+// Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+
 const inputEl = document.querySelector("#validation-input");
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.length == inputEl.getAttribute("data-length")) {
+  if (
+    event.currentTarget.value.length ===
+    Number(inputEl.getAttribute("data-length"))
+  ) {
     inputEl.classList.add("valid");
     inputEl.classList.remove("invalid");
   } else {
